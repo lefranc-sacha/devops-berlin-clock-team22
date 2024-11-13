05ea0aaecf7e13bf04bcebb8c7649051bc272138
daniiii2 <122824902+daniiii2@users.noreply.github.com>
2024-11-13 01:28:42 +0100

    refactor: fiveMinutesLine method for all cases

--- src/app.js
+++ src/app.js
@@ -7,15 +7,16 @@ export class Main{
         return "Y".repeat(onLamps) + "O".repeat(4 - onLamps);
     }
     fiveMinutesLine(minutes) {
-
-        if (minutes === 0) return "OOOOOOOOOOO";
-
-        if (minutes === 5) return "YOOOOOOOOOO";
-
-        if (minutes === 30) return "YYRYYROOOOO";
-        
-        if (minutes === 55) return "YYRYYRYYRYY";
-    
-        return "OOOOOOOOOOO";
+        const onLamps = Math.floor(minutes / 5);
+        let lamps = "";
+        for (let i = 1; i <= 11; i++) {
+          if (i <= onLamps) {
+            lamps += (i % 3 === 0) ? "R" : "Y";
+          } else {
+            lamps += "O";
+          }
+        }
+        return lamps;
       }
+      
 }
